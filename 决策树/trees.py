15aa6439f49ecca5b770868c4fca60a7371b6e01
juechen-zzz <240553516@qq.com>
2019-01-15 16:37:13 +0800

    2019-1-15

---
+++ 决策树/trees.py
@@ -0,0 +1,24 @@
+##决策树算法
+
+from math import log
+
+#创建一个简单数据集
+def createDataSet():
+	dataSet = [[1,1,'yes'],[1,1,'yes'],[1,0,'no'],[0,1,'no'],[0,1,'no']]
+	labels = ['no surfacing','flippers']
+	return dataSet,labels
+
+#计算给定数据集的香农熵
+def calcShannonEnt(dataSet):
+	numEntries = len(dataSet)
+	labelCounts = {}
+	for featVec in dataSet:
+		currentLabel = featVec[-1]
+		if currentLabel not in labelCounts.keys():
+			labelCounts[currentLabel] = 0
+		labelCounts[currentLabel] += 1
+	shannonEnt = 0.0
+	for key in labelCounts:
+		prob = float(labelCounts[key]) / numEntries
+		shannonEnt -= prob*log(prob,2)
+	return shannonEnt
