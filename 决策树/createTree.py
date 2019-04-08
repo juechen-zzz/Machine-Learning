f85816e0d5bce69db789c02a2532511764098915
juechen-zzz <240553516@qq.com>
2019-04-08 22:42:20 +0800

    change createTree

--- 决策树/createTree.py
+++ 决策树/createTree.py
@@ -1,6 +1,7 @@
 # 创建树的函数代码
 
 def createTree(dataSet, labels):
+    # 类别标签列表
     classList = [example[-1] for example in dataSet]
     # 类别完全相同则停止继续划分
     if classList.count(classList[0]) == len(classList):
@@ -8,12 +9,16 @@ def createTree(dataSet, labels):
     # 遍历完所有特征时返回出现次数最多的类别
     if len(dataSet[0]) == 1:
         return majorityCnt(classList)
+    # 获得最佳特征的下标和名称
     bestFeat = chooseBestFeatureToSplit(dataSet)
     bestFeatLabel = labels[bestFeat]
+    # 以最佳特征为根节点创建子树
     myTree = {bestFeatLabel: {}}
-    # 得到列表包含的所有属性值
+    # 在特征名列表中删除最佳特征(创建节点需要消耗特征)
     del (labels[bestFeat])
+    # 获得最佳特征的所有可能值
     featValues = [example[bestFeat] for example in dataSet]
+    # 去除重复的值
     uniqueVals = set(featValues)
     # 遍历当前选择特征包含的所有属性值
     for value in uniqueVals:
