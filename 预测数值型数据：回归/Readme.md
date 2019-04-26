dbb20b42a633b430964d5b6663887b1ab33d39e0
juechen-zzz <240553516@qq.com>
2019-04-26 11:08:24 +0800

    add in 2019-4-26

---
+++ 预测数值型数据：回归/Readme.md
@@ -0,0 +1,6 @@
+# Code structure
+
+* regression.py is the main algorithm. The rest of the file is an understanding of each function.
+
+![image](https://github.com/juechen-zzz/Machine-Learning/blob/master/%E5%86%B3%E7%AD%96%E6%A0%91/Rendering.jpg)
+
--- 预测数值型数据：回归/regression.py
+++ 预测数值型数据：回归/regression.py
@@ -26,3 +26,28 @@ def standRegres(xArr, yArr):
     ws = xTx.I * (xMat.T * yMat)
     return ws
 
+# 局部加权线性回归函数
+def lwlr(testPoint, xArr, yArr, k = 1.0):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye((m)))
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = exp(diffMat * diffMat.T / (-2.0 * k ** 2))
+    xTx = xMat.T * (weights * xMat)
+    if linalg.det(xTx) == 0.0:
+        print('this matrix is singular, can not do inverse')
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+def rssError(yArr, yHatArr):
+    return ((yArr - yHatArr) ** 2).sum()
